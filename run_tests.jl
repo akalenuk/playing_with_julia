d85fbf1c7894824553e3e62560f8c0f300c21201
Oleksandr Kaleniuk <akalenuk@gmail.com>
2018-12-09 11:38:02 +0200

    interpolation bug

--- run_tests.jl
+++ run_tests.jl
@@ -14,27 +14,37 @@ for i in 1:bump_map_h
   end
 end
 
+bump_from_uv = function(uv)
+  # bump-map coordinates
+  j = uv[1] * bump_map_w
+  i = uv[2] * bump_map_h
+  j0 = 1 + floor(Int, j)
+  j1 = min(1 + floor(Int, j) + 1, bump_map_w)
+  i0 = 1 + floor(Int, i)
+  i1 = min(1 + floor(Int, i) + 1, bump_map_h)
+#  bump = bump_map[i0, j0]
+  bump = bump_map[i1, j1] * (j - j0) * (i - i0) 
+       + bump_map[i1, j0] * (j1 - j) * (i - i0) 
+       + bump_map[i0, j0] * (j1 - j) * (i1 - i) 
+       + bump_map[i0, j1] * (j - j0) * (i1 - i) 
+  return bump
+end
+
+for u in 0:0.01:1
+  println(bump_from_uv((u, 0.0625)))
+end
+
+
 distance_function = function(x) 
   d = sqrt(x[1]^2 + x[2]^2 + x[3]^2) - 10 
   uv = (x[1] / 10., x[2] / 10.)
   if uv[1] > 0. && uv[1] < 1. && uv[2] > 0. && uv[2] < 1.
-    # bump-map coordinates
-    j = uv[1] * bump_map_w
-    i = uv[2] * bump_map_h
-    j0 = 1 +  floor(Int, j)
-    j1 = 1 + (floor(Int, j) + 1) % bump_map_w
-    i0 = 1 +  floor(Int, i)
-    i1 = 1 + (floor(Int, i) + 1) % bump_map_h
-    bump = bump_map[i0, j0] * (j - j0) * (i - i0) 
-         + bump_map[i0, j1] * (j1 - j) * (i - i0) 
-         + bump_map[i1, j1] * (j1 - j) * (i1 - i) 
-         + bump_map[i1, j0] * (j - j0) * (i1 - i) 
-    d -= bump
+    d -= bump_from_uv(uv)
   end
   return d
 end
 gradient_function = SurfaceMesh.distance_to_gradient_operator(distance_function)
-(vertexes, faces) = SurfaceMesh.build_3d_mesh((-10, -10, 5), (10, 10, 10), 0.25, distance_function, gradient_function)
+(vertexes, faces) = SurfaceMesh.build_3d_mesh((-10, -10, 5), (10, 10, 11), 0.25, distance_function, gradient_function)
 
 open("test.obj", "w") do file
   write(file, ObjIO.str_from_vertexes(vertexes))
