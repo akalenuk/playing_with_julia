cb2862445d4282378ddd017f702373cb410b6b2d
Oleksandr Kaleniuk <akalenuk@gmail.com>
2019-05-18 08:10:16 +0300

    voxel state

--- sploxels.jl
+++ sploxels.jl
@@ -1,13 +1,18 @@
 module Sploxels
+    @enum VoxelState begin
+        inside = -1
+        on_margin = 0
+        outside = 1
+    end
 
     struct Sploxel
         g_shift :: Array{Float64,1}
         g_value :: Array{Float64,1}
+        voxel_state :: VoxelState
         topology :: Array{Array{Bool,1},1}
         # topology = connections to the neighboring sploxels
         # x-axis -1:+1
         # y-axis -1:+1
         # z-axis -1:+1
     end
-
 end
